2b796b2dd167731d2dc93da0f658c732448fce75
gusgad <gusgadirov@gmail.com>
2017-07-28 14:50:43 +0300

    Load in the dataset

---
+++ train.py
@@ -0,0 +1,13 @@
+import pickle
+
+training_file = "training_data/train.p"
+testing_file = "test_data/test.p"
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+
+X_train, y_train = train['features'], train['labels']
+X_test, y_test = test['features'], test['labels']
+print("Data loaded")
