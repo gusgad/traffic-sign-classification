ed2f81e857bf6cb5025cf4bc36ac23ab698d5230
gusgad <gusgadirov@gmail.com>
2017-07-30 20:26:28 +0300

    Normalize and split for NN

--- train.py
+++ train.py
@@ -34,7 +34,6 @@ for i in range(n_test):
     idx = int(y_test[i])
     count_test[idx] +=1
 
-print('count train class values', count_train[2])
 
 # grouping for counting class numbers
 class_groups = np.zeros(3)
@@ -46,7 +45,6 @@ for i in range(n_classes):
     else:
         class_groups[2] += 1
 
-print('class groups by quantity: ', class_groups)
 
 # upper and lower limit of each class_group
 limits = np.zeros([n_classes, 2])
@@ -56,9 +54,6 @@ limits[0, 1] = count_train[0]
 for i in range(1, n_classes):
     limits[i, 0] = limits[i - 1, 1] + 1
     limits[i, 1] = limits[i, 0] + count_train[i] - 1
-
-#print('limits first class', limits[3])
-print('limits shape: ', limits.shape)
 
 
 ### normalization
@@ -75,47 +70,70 @@ def grayscale_array(X_Arr):
         X_out[i,:,:,0] = to_grayscale(img)
     return X_out
 
-grayscaled = grayscale_array(X_train)
 
 # normalizing sample number per class_groups
-#Distort
 X_out = X_train
 y_out = y_train
 
 for n in range(n_classes): #For each class
-    if count_train[n] <1000:
+    if (count_train[n] < 1000):
         #Add twice
-        tini = int(limits[n,0])
-        tend = int(limits[n,1])
-        X_swap1 =  X_train[tini:tend,:,:,:]
-        X_swap2 =  X_train[tini:tend,:,:,:]
-        y_d1 = y_train[tini:tend]
-        y_d2 = y_train[tini:tend]
+        cl_start = int(limits[n, 0])
+        cl_end = int(limits[n, 1])
+        X_swap1 =  X_train[cl_start:cl_end,:,:,:]
+        X_swap2 =  X_train[cl_start:cl_end,:,:,:]
+        y_d1 = y_train[cl_start:cl_end]
+        y_d2 = y_train[cl_start:cl_end]
 
+        X_out = np.concatenate([X_out, X_swap1, X_swap2])
+        y_out = np.concatenate([y_out, y_d1, y_d2])
 
-        X_out = np.concatenate([X_out,X_swap1,X_swap2])
-        y_out = np.concatenate([y_out,y_d1,y_d2])
+    elif (count_train[n] < 2000):
+        cl_start = int(limits[n, 0])
+        cl_end = int(limits[n, 1])
+        X_swap =  X_train[cl_start:cl_end,:,:,:]
+        y_d = y_train[cl_start:cl_end]
 
-    elif count_train[n] <2000:
-        tini = int(limits[n,0])
-        tend = int(limits[n,1])
-        X_swap =  X_train[tini:tend,:,:,:]
-        y_d1 = y_train[tini:tend]
-
-        X_out = np.concatenate([X_out,X_swap])
-        y_out = np.concatenate([y_out,y_d1])
+        X_out = np.concatenate([X_out, X_swap])
+        y_out = np.concatenate([y_out, y_d])
 
     else:
-        tini = int(limits[n,0])
-        tend = int((limits[n,1]+ limits[n,0])/2)
-        X_swap =  X_train[tini:tend,:,:,:]
-        y_d1 = y_train[tini:tend]
+        cl_start = int(limits[n, 0])
+        cl_end = int((limits[n, 1] + limits[n, 0]) / 2)
+        X_swap =  X_train[cl_start:cl_end,:,:,:]
+        y_d = y_train[cl_start:cl_end]
 
-        X_out = np.concatenate([X_out,X_swap])
-        y_out = np.concatenate([y_out,y_d1])
-
-
-print('X_train_multiplied', X_out.shape)
+        X_out = np.concatenate([X_out, X_swap])
+        y_out = np.concatenate([y_out, y_d])
 
 
-#print('X_train swapped', X_train_swapped2[0])
+X_train_gray = grayscale_array(X_out)
+X_train_norm= X_train_gray / 255
+
+
+# shuffling training and test sets
+from sklearn.utils import shuffle
+
+X_train_shuffled, y_train_shuffled = shuffle(X_train_norm, y_out, random_state = 3)
+X_test_shuffled, y_test_suffled = shuffle(X_test, y_test, random_state = 3)
+
+#split  training/validation/
+n_train = len(X_train_shuffled)
+valid_range = int(n_train*0.8)
+
+X_train_ini = (X_train_shuffled[0:valid_range + 1])
+X_valid_ini = (X_train_shuffled[valid_range + 1:n_train + 1])
+y_train_ini = (y_train_shuffled[0:valid_range + 1])
+y_valid_ini = (y_train_shuffled[valid_range + 1:n_train + 1])
+
+print(X_train_ini.shape)
+print(X_valid_ini.shape)
+
+
+
+
+
+
+
+
+
