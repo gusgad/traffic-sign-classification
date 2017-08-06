7c01838563ed532683046394d21d259f910d1ede
gusgad <gusgadirov@gmail.com>
2017-08-06 19:10:55 +0300

    Test and run the model

--- train.py
+++ train.py
@@ -140,7 +140,7 @@ def model(x):
     sigma = 0.1
 
     #======================================================================
-    # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x6.
+    # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28x20.
     w1 = tf.Variable(tf.truncated_normal([5,5,1,inflated[0]], mean = mu, stddev = sigma))
     b1 = tf.Variable(tf.zeros(inflated[0]))
 
@@ -150,13 +150,13 @@ def model(x):
     # activation
     l1_act = tf.nn.relu(l1_conv, name = 'l1_act')
 
-    # Pooling. Input = 28x28x6. Output = 14x14x6.
+    # Pooling. Input = 28x28x20. Output = 14x14x20.
     l1_pool = tf.nn.max_pool(l1_act, [1,2,2,1],[1,2,2,1],'VALID', name = 'l1_pool')
     l1_pool =tf.nn.dropout(l1_pool,.5)
 
 
     #======================================================================
-    # Layer 2: Convolutional. Output = 10x10x16.
+    # Layer 2: Convolutional. Input = 14x14x20. Output = 10x10x48.
 
     w2 = tf.Variable(tf.truncated_normal([5,5,inflated[0],inflated[1]], mean = mu, stddev = sigma))
     b2 = tf.Variable(tf.zeros(inflated[1]))
@@ -168,31 +168,27 @@ def model(x):
     l2_act = tf.nn.relu(l2_conv, name = 'l2_act')
 
 
-    # Pooling. Input = 10x10x16. Output = 5x5x16.
-    l2_pool = tf.nn.max_pool(l2_act, [1,2,2,1],[1,2,2,1],'VALID', name = 'l2_pool')
-
-
     #======================================================================
-    # Layer 3: Convolutional. Output = xx-xx-xx.
+    # Layer 3: Convolutional. Input = 10x10x48. Output = 6x6x56.
 
     w3 = tf.Variable(tf.truncated_normal([5,5,inflated[1],inflated[2]], mean = mu, stddev = sigma))
     b3 = tf.Variable(tf.zeros(inflated[2]))
 
-    l3_conv = tf.nn.conv2d(l2_pool, w3, strides = [1,1,1,1], padding = 'VALID', name = 'l3_conv')
+    l3_conv = tf.nn.conv2d(l2_act, w3, strides = [1,1,1,1], padding = 'VALID', name = 'l3_conv')
     l3_conv = tf.nn.bias_add(l3_conv, b3)
 
     # activation
-    l2_act = tf.nn.relu(l3_conv, name = 'l3_act')
+    l3_act = tf.nn.relu(l3_conv, name = 'l3_act')
 
 
-    # Pooling. Input = 10x10x16. Output = 5x5x16.
+    # Pooling. Input = 6x6x56. Output = 3x3x56.
     l3_pool = tf.nn.max_pool(l3_act, [1,2,2,1],[1,2,2,1],'VALID', name = 'l3_pool')
     l3_pool =tf.nn.dropout(l3_pool,.5)
 
 
     #===============================================================
-    # Flatten. Input = 5x5x16. Output = 400.
-    nflat = int(5*5*inflated[2])
+    # Flatten. Input = 3x3x56. Output = 400.
+    nflat = int(3*3*inflated[2])
     x_flatten = tf.reshape(l3_pool, [-1,nflat], name = 'x_flatten')
 
     # Layer 4: Fully Connected. Input = 400. Output = 120.
@@ -202,21 +198,102 @@ def model(x):
     # activation
     l4 = tf.add(tf.matmul(x_flatten,w4), b4, name = 'l4')
     l4 = tf.nn.relu(l4)
-    l4 = tf.nn.dropout(l4,.8)
+    l4 = tf.nn.dropout(l4,.5)
 
     # Layer 5: Fully Connected. Input = 120. Output = 84.
     w5 = tf.Variable(tf.truncated_normal([120,84], mean = mu, stddev = sigma))
     b5 = tf.Variable(tf.zeros(84))
 
     # activation
-    l5 = tf.add(tf.matmul(l4, w5),b4, name = 'l5')
+    l5 = tf.add(tf.matmul(l4, w5),b5, name = 'l5')
     l5 = tf.nn.relu(l5)
-
 
     # Layer 6: Fully Connected. Input = 84. Output = 43.
     w6 = tf.Variable(tf.truncated_normal([84,43], mean = mu, stddev = sigma))
     b6 = tf.Variable(tf.zeros(43))
 
-    logits = tf.add(tf.matmul(l5, w6), b5, name = 'l6')
+    logits = tf.add(tf.matmul(l5, w6), b6, name = 'l6')
     
     return logits
+
+
+EPOCHS = 20
+BATCH_SIZE = 40
+rate = 0.001
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, 43)
+
+logits = model(x)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = one_hot_y)
+loss_operation = tf.reduce_mean(cross_entropy)
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
+
+#Model evaluation
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+saver = tf.train.Saver()
+
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+
+from time import time
+start_time = time()
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train_ini)
+    
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_tref, y_tref = shuffle(X_train_ini, y_train_ini)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_tref[offset:end], y_tref[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+
+        validation_accuracy = evaluate(X_valid_ini, y_valid_ini)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+
+    print("Final Validation Accuracy = {:.3f}".format(validation_accuracy))
+    saver.save(sess, 'model')
+    print("Model saved")
+
+
+
+end_time = time()
+time_taken = end_time - start_time # time_taken is in seconds
+
+hours, rest = divmod(time_taken,3600)
+minutes, seconds = divmod(rest, 60)
+
+print ("Time: ", hours, "h, ", minutes, "min, ", seconds, "s ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
